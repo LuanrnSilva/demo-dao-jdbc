ba5c50027e8d429b8db3c31b9c6903b794efff4a
luan <luanrenatos@hotmail.com>
2024-09-23 20:45:38 -0300

    update implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -29,9 +29,15 @@ public class Program {
 		for (Seller sell : list) {
 			System.out.println(sell);
 		}
-		System.out.println("\n=== TEST 4: seller insert ===");
-		Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, department);
-		sellerDao.insert(newSeller);
-		System.out.println("Inserted! New id = " + newSeller.getId());
+//		System.out.println("\n=== TEST 4: seller insert ===");
+//		Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, department);
+//		sellerDao.insert(newSeller);
+//		System.out.println("Inserted! New id = " + newSeller.getId());
+		
+		System.out.println("\n=== TEST 5: seller update ===");
+		seller = sellerDao.findById(1);
+		seller.setName("Martha Waine");
+		sellerDao.update(seller);
+		System.out.println("Update completed");
 	}
 }
